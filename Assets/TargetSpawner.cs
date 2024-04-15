07f1f5ba012548da6e7bc67c3574cb9ff8e70262
alexandreafi <afi.alexandre@student-cs.fr>
2024-04-15 16:02:35 +0200

    target implementation

---
+++ Assets/TargetSpawner.cs
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class TargetSpawner : MonoBehaviour
+{
+    public targetBehaviour Package;
+    public GameObject PackagePrefab;
+    public GameObject finishLine;
+    private int counter = 4;
+    
+
+    public static Vector3 FindRandomLocation()
+    {
+
+        var randomPoint = new Vector3();
+        randomPoint.x = Random.Range(0.0f, 2.0f);
+        randomPoint.y = 1;
+        randomPoint.z = Random.Range(0.0f, 2.0f);
+
+        return randomPoint;
+    }
+
+    public void SpawnPackage()
+    {
+        var packageClone = GameObject.Instantiate(PackagePrefab);
+        packageClone.transform.position = FindRandomLocation();
+
+        Package = packageClone.GetComponent<targetBehaviour>();
+    }
+/*
+    public void SpawnFinishLine()
+    {
+        var finishClone = GameObject.Instantiate(finishLine);
+        finishClone.transform.position = FindRandomLocation();
+
+        FinishLine = finishClone.GetComponent<FinishLineBehaviour>();
+    }
+*/
+    private void Update()
+    {
+        if (counter != 0)
+        {
+            if (Package == null)
+            {
+                SpawnPackage();
+                counter--;
+            }
+            var packagePosition = Package.gameObject.transform.position;
+            packagePosition.Set(packagePosition.x, packagePosition.y, packagePosition.z);
+        }
+/*        else
+        {
+            if (FinishLine == null)
+            {
+                SpawnFinishLine();
+            }
+            var finishPosition = finishLine.gameObject.transform.position;
+            finishPosition.Set(finishPosition.x, finishPosition.y, finishPosition.z);
+        }
+*/
+
+    }
+}
---
+++ Assets/sphereBehaviour.cs
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections;
+using UnityEngine;
+
+/**
+ * Our car will track a reticle and collide with a <see cref="targetBehaviour"/>.
+ */
+/**
+ * Our car will track a reticle and collide with a <see cref="FinishLineBehaviour"/>.
+ */
+public class sphereBehaviour : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        var Package = other.GetComponent<targetBehaviour>();
+            if (Package != null)
+            {
+                Destroy(other.gameObject);
+            }
+        
+    }
+}
